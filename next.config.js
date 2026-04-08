56b24bd50f6f80745fbd10e7386c4d299e331a90
marhoumadnane1-sudo <marhoumadnane1@gmail.com>
2026-04-08 15:25:59 +0200

    chore: add output export and basePath for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,49 +1,52 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'plus.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'iprenders.blob.core.windows.net',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.netdirector.auto',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.ytimg.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.shutterstock.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'pre-webunwto.s3.eu-west-1.amazonaws.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'mnarcastle.ma',
-      },
-      {
-        protocol: 'https',
-        hostname: 'primemoroccotours.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.topmorocco.com',
-      },
-    ],
-  },
+    output: 'export',
+    basePath: '/adnane-tourwebsite-',
+    images: {
+          unoptimized: true,
+          remotePatterns: [
+            {
+                      protocol: 'https',
+                      hostname: 'images.unsplash.com',
+            },
+            {
+                      protocol: 'https',
+                      hostname: 'plus.unsplash.com',
+            },
+            {
+                      protocol: 'https',
+                      hostname: 'iprenders.blob.core.windows.net',
+            },
+            {
+                      protocol: 'https',
+                      hostname: 'images.netdirector.auto',
+            },
+            {
+                      protocol: 'https',
+                      hostname: 'i.ytimg.com',
+            },
+            {
+                      protocol: 'https',
+                      hostname: 'www.shutterstock.com',
+            },
+            {
+                      protocol: 'https',
+                      hostname: 'pre-webunwto.s3.eu-west-1.amazonaws.com',
+            },
+            {
+                      protocol: 'https',
+                      hostname: 'mnarcastle.ma',
+            },
+            {
+                      protocol: 'https',
+                      hostname: 'primemoroccotours.com',
+            },
+            {
+                      protocol: 'https',
+                      hostname: 'www.topmorocco.com',
+            },
+                ],
+    },
 };
 
 module.exports = nextConfig;
